4817d5766897d88fbfe310c9278815231a839f7a
Yuvraj Sharma <yuvraj.k.link33@gmail.com>
2026-03-15 21:19:03 +0530

    Update Rice_app.py

--- Rice_app.py
+++ Rice_app.py
@@ -23,14 +23,8 @@ st.title("🌾 AI Based Rice Leaf Disease Detection System")
 st.markdown("""
 Deep Learning based system to detect **rice leaf diseases**.
 
-### Models Used
-• AlexNet & CNN (Base model)\
-• VGG16 (Best Model)\
-• ResNet50\
-• MobileNetV2\
-• EfficientNetB0\
-• Ensemble CNN Model
-               
+### Models Used - AlexNet | VGG16 (Best) | ResNet50 | MobileNetV2 (Runner-up) | EfficentNetB0 | Ensemble CNN 
+
 
 Includes **Explainable AI using Grad-CAM**.
 """)
